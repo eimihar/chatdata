4f4fb660c9121af9269f3b6dc20a70603d9b58e1
Ahmad Rahimie <newrehmi@gmail.com>
2023-04-05 02:56:57 +0800

    nested, and middleware example

--- exp/index.ts
+++ exp/index.ts
@@ -3,9 +3,22 @@ import express from "express";
 const app = express();
 const port = 3030;
 
-app.get('/', (req, res) => {
-    return res.send('Hello world');
+const shopRouter = express.Router({mergeParams: true});
+
+// app.get('/', (req, res) => {
+//     return res.send('Hello world');
+// })
+
+app.use('/shop/:id', shopRouter);
+
+shopRouter.use((req, res, next) => {
+    return next();
 })
+
+shopRouter.get('/', (req, res) => {
+    return res.send('yeah');
+})
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
