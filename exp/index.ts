a9af7cb93168c71407f5344a33f4e67d4289b094
Ahmad Rahimie <newrehmi@gmail.com>
2023-04-05 02:58:33 +0800

    Update index.ts

--- exp/index.ts
+++ exp/index.ts
@@ -5,13 +5,16 @@ const port = 3030;
 
 const shopRouter = express.Router({mergeParams: true});
 
-// app.get('/', (req, res) => {
-//     return res.send('Hello world');
-// })
+app.get('/', (req, res) => {
+    return res.send('Hello world');
+})
 
 app.use('/shop/:id', shopRouter);
 
 shopRouter.use((req, res, next) => {
+    if (req.params.id == "12")
+        return res.send('it is within middleware');
+
     return next();
 })
 
