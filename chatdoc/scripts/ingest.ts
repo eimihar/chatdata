f170ea5a24a8a2d442b00bbcad58d4a523e9e544
Ahmad Rahimie <newrehmi@gmail.com>
2023-04-06 05:37:41 +0800

    got it to work finally

--- chatdoc/scripts/ingest.ts
+++ chatdoc/scripts/ingest.ts
@@ -1,17 +1,27 @@
 import {PDFLoader} from "langchain/document_loaders";
 import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
 import {PineconeStore} from "langchain/vectorstores";
+import {OpenAI} from "langchain";
+import moment from "moment";
 
 export const run = async () => {
-    const pdf = 'docs/sample.pdf';
-    const loader = new PDFLoader(pdf);
-    const rawDocs = await loader.load();
+    // const pdf = 'docs/sample.pdf';
+    // // const loader = new PDFLoader(pdf);
+    // // const rawDocs = await loader.load();
     const textSplitter = new RecursiveCharacterTextSplitter({
         chunkSize: 1000,
         chunkOverlap: 200
     });
 
-    new PineconeStore()
+    console.log(moment())
+
+    // const oai = new OpenAI();
+
+    // const docs = await textSplitter.splitDocuments(await loader.load())
+
+    // console.log(docs);
+
+    // console.log((await loader.))
 
     // await PineconeStore.fromDocuments(textSplitter.splitDocuments())
 }
--- chatdoc/server.ts
+++ chatdoc/server.ts
@@ -1,10 +1,26 @@
 import express from "express";
+import {PDFLoader} from "langchain/document_loaders";
+import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
 
 const app = express();
 const port = 10106;
 
 app.get('/', (req, res) => {
     return res.send('Hello world');
+})
+
+app.get('/ingest', async () => {
+    const pdf = 'docs/sample.pdf';
+    const loader = new PDFLoader(pdf);
+    // const rawDocs = await loader.load();
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 200
+    });
+
+    const docs = await textSplitter.splitDocuments(await loader.load())
+
+    console.log(docs);
 })
 
 app.listen(port, () => {
